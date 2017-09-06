5e7b0775894ecfefec55567ca12087a7bd2e0f94
quyet <quyet@whl.travel>
2017-09-06 11:00:27 +0700

    js and css

--- application/views/template/index.php
+++ application/views/template/index.php
@@ -2,14 +2,20 @@
 <head>
     <title>{blog_title}</title>
     <?php echo link_tag('css/styles.css'); ?>
+
+    <script type = 'text/javascript' src = "<?php echo base_url();
+    ?>js/sample.js"></script>
 </head>
 <body>
+<a href='javascript:test()'>Click Here</a> to execute the javascript function.
 <h3>{blog_heading}</h3>
 <div class="cls">Class</div>
 {blog_entries}
 <h5>{title}</h5>
 <p>{body}</p>
 {/blog_entries}
+
+
 
 </body>
 </html>
