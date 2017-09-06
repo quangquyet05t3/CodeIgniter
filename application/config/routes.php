9250d1eeef4a93fde373aff785c96448e27fa66e
quyet <quyet@whl.travel>
2017-09-06 09:58:39 +0700

    template

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,5 +5,6 @@ $route['default_controller'] = 'welcome';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['pages'] = 'pages';
+$route['template'] = 'template';
 $route['(:any)'] = 'pages/view/$1';
 
---
+++ application/views/template/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>{blog_title}</title>
+    <?php echo link_tag('css/styles.css'); ?>
+</head>
+<body>
+<h3>{blog_heading}</h3>
+<div class="cls">Class</div>
+{blog_entries}
+<h5>{title}</h5>
+<p>{body}</p>
+{/blog_entries}
+
+</body>
+</html>
